33d930f43de679f2944b45bbbd4a087e54a1b7f0
papparazzo <pappi-@gmx.de>
2017-05-15 23:26:53 +0200

    src added

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+/*
+ *  Project:    moba-sniffer
+ *
+ *  Copyright (C) 2016 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#include <iostream>
+
+#include <gtkmm/application.h>
+
+#include <moba/helper.h>
+
+#include "frmmain.h"
+#include "config.h"
+
+int main(int argc, char *argv[]) {
+    moba::setCoreFileSizeToULimit();
+
+    auto app = Gtk::Application::create(argc, argv, "org.moba.sniffer");
+
+    FrmMain frmMain;
+    frmMain.set_title("CS2 Sniffer");
+    frmMain.set_border_width(10);
+    frmMain.set_default_size(400, 200);
+
+    return app->run(frmMain);
+}
