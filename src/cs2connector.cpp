5b4eb6397da8d7a0d18c18e3a2781f1d08379795
papparazzo <pappi-@gmx.de>
2017-06-28 23:19:56 +0200

    wip...

---
+++ src/cs2connector.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   cs2connector.cpp
+ * Author: stefan
+ *
+ * Created on June 15, 2017, 7:20 PM
+ */
+
+#include "cs2connector.h"
+
+/*
+cs2connector::cs2connector() {
+}
+
+cs2connector::cs2connector(const cs2connector& orig) {
+}
+
+cs2connector::~cs2connector() {
+}
+*/
+
+
