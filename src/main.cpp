d0474e4fcadcb56dffbcf21058a3c6a686ea25f8
papparazzo <pappi-@gmx.de>
2022-12-30 14:59:54 +0100

    wip...

--- src/main.cpp
+++ src/main.cpp
@@ -19,45 +19,112 @@
  */
 
 #include <moba-common/helper.h>
-#include <moba-common/log.h>
 #include <iostream>
+#include <getopt.h>
 
-#include <cstring>
 #include <string>
 
-#include "moba/cs2reader.h"
-#include "moba/cs2writer.h"
-#include "moba/cs2cancommand.h"
-#include "moba/cs2utils.h"
-#include "moba/printcancommand.h"
-#include "moba/configreader.h"
-#include "moba/configloklistreader.h"
-#include "s88sniffer.h"
-
+#include <config.h>
+#include "handler.h"
 #include <map>
 #include <functional>
 #include <memory>
 
-int main(int argc, char *argv[]) {
+namespace {
+    moba::AppData appData = {
+        PACKAGE_NAME,
+        moba::Version{PACKAGE_VERSION},
+        __DATE__,
+        __TIME__,
+        "",
+        0
+    };
 
-    CS2Writer cs2writer;
-    CS2Reader cs2reader;
+    struct CmdLineArguments {
+        std::uint32_t localId;
+    };
+}
 
-    cs2writer.connect("192.168.178.38");
-    cs2reader.connect();
+void printHelp() {
+    std::cout << std::endl;
+    std::cout << "-i, --localid   locId" << std::endl;
+    std::cout << "-h, --help      shows this help" << std::endl;
+    std::cout << "-v, --version   shows version-info" << std::endl;
+}
 
-    //PrintCanCommand printer{};
+bool parseArguments(int argc, char** argv, CmdLineArguments &args) {
+    static struct option longOptions[] = {
+        {"localid",   required_argument, 0,    'i'},
+        {"help",      no_argument,       0,    'h'},
+        {"version",   no_argument,       0,    'v'},
 
-    //auto loklist = std::make_shared<ConfigLoklistReader>();
 
-    //ConfigReader configReader{};
-    //configReader.addHandler(loklist);
+        {"data",      required_argument, 0,    'd'},
+        {"action",    required_argument, 0,    'a'},
+        {NULL,        0,                 NULL,  0 }
+    };
 
-    S88Sniffer sniffer;
+    int optionIndex = 0;
 
-//    cs2writer.send(getLokStat());
-    //cs2writer.send(getLokList());
-    //cs2writer.send(getMagList());
+    while (true) {
+        int c = getopt_long(argc, argv, "i:hv", longOptions, &optionIndex);
+        if(c == -1) {
+            break;
+        }
+
+        switch (c) {
+            case 'i':
+                args.localId = std::stoul(optarg);
+                break;
+
+            case 'h':
+                printHelp();
+                return true;
+
+            case 'v':
+                moba::printAppData(appData);
+                return true;
+
+
+                /*
+            case 'k':
+             *
+                args.key = atoi(optarg);
+                break;
+
+            case 'd':
+                args.data = std::string(optarg);
+                break;
+             */
+
+        }
+    }
+    return false;
+}
+
+int main(int argc, char** argv) {
+    Handler handler;
+
+    handler.printConfigList("lokstatus");
+
+    //handler.printLoklist();
+
+    CmdLineArguments args = {
+        0
+    };
+
+    try {
+        if(parseArguments(argc, argv, args)) {
+            return EXIT_SUCCESS;
+        }
+
+    } catch(std::exception &e) {
+        std::cout << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    // return EXIT_SUCCESS;
+
+
 /*
     cs2writer.send(setSwitch(convertMMToLocId(6), true, true));
     usleep(50000);
@@ -90,33 +157,10 @@ int main(int argc, char *argv[]) {
 //    usleep(50000);
 //    cs2writer.send(setSwitch(convertMMToLocId(35), true, false));
 
-//    ConfigParser parser;
-
-    cs2writer.send(setLocFunction(16390, 0, true));
+//    cs2writer.send(setLocFunction(16390, 0, true));
 //    cs2writer.send(setLocFunction(16390, 2, true));
 //    sleep(7);
 //    cs2writer.send(setLocSpeed(16390, 500));
 
-    while(true) {
-        CS2CanCommand data = cs2reader.read();
-
-        sniffer.handleCanCommand(data);
-
-        //configReader.handleCanCommand(data);
-
-        /*
-        if(loklist->locomotives.size()) {
-            for(auto const &ent1 : loklist->locomotives) {
-                std::cout << "name: " << ent1.second->name << std::endl;
-
-                //for(auto const &ent2 : ent1.second) {
-
-               // }
-            }
-        }
-
-         */
-       // printer.handleCanCommand(data);
-    }
 }
 
