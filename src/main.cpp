f2634300c663543b088b9d87d5f3e6c6426102d2
papparazzo <pappi-@gmx.de>
2023-01-09 22:04:58 +0100

    wip...

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,19 @@
  */
 
 #include <moba-common/helper.h>
+
 #include <iostream>
 #include <getopt.h>
 
 #include <string>
 
 #include <config.h>
-#include "handler.h"
 #include <map>
 #include <functional>
 #include <memory>
+#include <libgen.h>
+
+#include "handler.h"
 
 namespace {
     moba::AppData appData = {
@@ -42,32 +45,40 @@ namespace {
 
     struct CmdLineArguments {
         std::uint32_t localId;
+        std::uint32_t function;
+        std::uint16_t speed;
     };
 }
 
-void printHelp() {
+void printHelp(const std::string &applName) {
+    std::cout << applName << " [s88|dump|loklist|lokstatus|lokomotive|function_on|function_off|speed]" << std::endl;
     std::cout << std::endl;
     std::cout << "-i, --localid   locId" << std::endl;
+    std::cout << "-s, --speed     speed" << std::endl;
+    std::cout << "-f, --function  function" << std::endl;
     std::cout << "-h, --help      shows this help" << std::endl;
     std::cout << "-v, --version   shows version-info" << std::endl;
 }
 
 bool parseArguments(int argc, char** argv, CmdLineArguments &args) {
+    if(argc < 2) {
+        printHelp(basename(argv[0]) );
+        return true;
+    }
+
     static struct option longOptions[] = {
         {"localid",   required_argument, 0,    'i'},
+        {"speed",     required_argument, 0,    's'},
+        {"function",  required_argument, 0,    'f'},
         {"help",      no_argument,       0,    'h'},
         {"version",   no_argument,       0,    'v'},
-
-
-        {"data",      required_argument, 0,    'd'},
-        {"action",    required_argument, 0,    'a'},
         {NULL,        0,                 NULL,  0 }
     };
 
     int optionIndex = 0;
 
     while (true) {
-        int c = getopt_long(argc, argv, "i:hv", longOptions, &optionIndex);
+        int c = getopt_long(argc, argv, "s:f:i:hv", longOptions, &optionIndex);
         if(c == -1) {
             break;
         }
@@ -78,52 +89,63 @@ bool parseArguments(int argc, char** argv, CmdLineArguments &args) {
                 break;
 
             case 'h':
-                printHelp();
+                printHelp(basename(argv[0]) );
                 return true;
 
             case 'v':
                 moba::printAppData(appData);
                 return true;
 
-
-                /*
-            case 'k':
-             *
-                args.key = atoi(optarg);
+            case 'f':
+                args.function = std::stoul(optarg);
                 break;
 
-            case 'd':
-                args.data = std::string(optarg);
+            case 's':
+                args.speed = std::stoul(optarg);
                 break;
-             */
-
         }
     }
     return false;
 }
 
 int main(int argc, char** argv) {
-    Handler handler;
-
-    handler.printConfigList("lokstatus");
-
-    //handler.printLoklist();
-
     CmdLineArguments args = {
         0
     };
 
-    try {
-        if(parseArguments(argc, argv, args)) {
-            return EXIT_SUCCESS;
-        }
+    std::string action{argv[1]};
 
+    if(parseArguments(argc, argv, args)) {
+        return EXIT_SUCCESS;
+    }
+
+    Handler handler;
+
+    try {
+        if(action == "s88") {
+            handler.printFeedBackAction();
+        } else if(action == "dump") {
+            handler.printCanCommands();
+        } else if(action == "loklist") {
+            handler.printLoklist();
+        } else if(action == "lokstatus") {
+            handler.printConfigList("lokstatus");
+        } else if(action == "lokomotive") {
+            handler.printConfigList("lokomotive");
+        } else if(action == "function_on") {
+            handler.setFunction(args.localId, args.function, true);
+        } else if(action == "function_off") {
+            handler.setFunction(args.localId, args.function, false);
+        } else if(action == "speed") {
+            handler.setSpeed(args.localId, args.speed);
+        } else {
+            std::cout << "unknown command <" << action << "> given!" << std::endl;
+        }
     } catch(std::exception &e) {
         std::cout << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-    // return EXIT_SUCCESS;
-
+    return EXIT_SUCCESS;
 
 /*
     cs2writer.send(setSwitch(convertMMToLocId(6), true, true));
@@ -141,26 +163,6 @@ int main(int argc, char** argv) {
     cs2writer.send(setSwitch(convertMMToLocId(6), false, true));
     usleep(50000);
     cs2writer.send(setSwitch(convertMMToLocId(6), false, false));
-
  */
-
-
-//    cs2writer.send(setSwitch(convertMMToLocId(7), true, true));
-//    usleep(50000);
-//    cs2writer.send(setSwitch(convertMMToLocId(7), true, false));
-//    sleep(2);
-//    cs2writer.send(setSwitch(convertMMToLocId(34), true, true));
-//    usleep(50000);
-//    cs2writer.send(setSwitch(convertMMToLocId(34), true, false));
-//    sleep(2);
-//    cs2writer.send(setSwitch(convertMMToLocId(35), true, true));
-//    usleep(50000);
-//    cs2writer.send(setSwitch(convertMMToLocId(35), true, false));
-
-//    cs2writer.send(setLocFunction(16390, 0, true));
-//    cs2writer.send(setLocFunction(16390, 2, true));
-//    sleep(7);
-//    cs2writer.send(setLocSpeed(16390, 500));
-
 }
 
