8b1aabb48fb3d1f5ef1939cf2a848d19450bc2bf
papparazzo <pappi-@gmx.de>
2026-01-14 18:17:25 +0100

    improve main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,12 @@
 #include <memory>
 #include <libgen.h>
 
-#include "handler.h"
+#include <gtkmm/application.h>
+
+#include "moba/cs2reader.h"
+#include "moba/cs2writer.h"
+
+#include "frmmain.h"
 
 namespace {
     moba::AppData appData = {
@@ -40,122 +45,85 @@ namespace {
         "",
         0
     };
-
-    struct CmdLineArguments {
-        std::uint32_t localId;
-        std::uint32_t function;
-        std::uint16_t speed;
-    };
 }
 
-void printHelp(const std::string &applName) {
-    std::cout << applName << " [s88|dump|loklist|lokstat|loks|magstat|mags|function_on|function_off|speed|switch_green|switch_red]" << std::endl;
+struct CS2ContactData {
+    std::string host;
+    int portIn;
+    int portOut;
+};
+
+void printHelp(const std::string &appName, CS2ContactData &cs2ContactData) {
+    std::cout << appName << std::endl;
     std::cout << std::endl;
-    std::cout << "-i, --localid   locId" << std::endl;
-    std::cout << "-s, --speed     speed" << std::endl;
-    std::cout << "-f, --function  function" << std::endl;
-    std::cout << "-h, --help      shows this help" << std::endl;
-    std::cout << "-v, --version   shows version-info" << std::endl;
+    std::cout << "-h, --help          shows this help" << std::endl;
+    std::cout << "-v, --version       shows version-info" << std::endl;
+    std::cout << "-c, --cs2-host      host of the CentralStation (default: " << cs2ContactData.host << ")" << std::endl;
+    std::cout << "-i, --cs2-port-in   port of the CentralStation for incoming messages (default: " << cs2ContactData.portIn << ")" << std::endl;
+    std::cout << "-o, --cs2-port-out  port of the CentralStation for outgoing messages (default: " << cs2ContactData.portOut << ")" << std::endl;
 }
 
-bool parseArguments(int argc, char** argv, CmdLineArguments &args) {
-    if(argc < 2) {
-        printHelp(basename(argv[0]));
-        return true;
-    }
-
-    static struct option longOptions[] = {
-        {"localid",   required_argument, nullptr,    'i'},
-        {"speed",     required_argument, nullptr,    's'},
-        {"function",  required_argument, nullptr,    'f'},
-        {"help",      no_argument,       nullptr,    'h'},
-        {"version",   no_argument,       nullptr,    'v'},
-        {nullptr,     0,                 nullptr, 0 }
+bool parseArguments(const int argc, char *argv[], const moba::AppData &appData, CS2ContactData &cs2ContactData) {
+    static option longOptions[] = {
+        {"cs2-host",     required_argument, nullptr, 'c'},
+        {"cs2-port-in",  required_argument, nullptr, 'i'},
+        {"cs2-port-out", required_argument, nullptr, 'o'},
+        {"help",         no_argument,       nullptr, 'h'},
+        {"version",      no_argument,       nullptr, 'v'},
+        {nullptr,        0,                 nullptr, 0 }
     };
 
     int optionIndex = 0;
 
-    while (true) {
-        int c = getopt_long(argc, argv, "s:f:i:hv", longOptions, &optionIndex);
+    while(true) {
+        const int c = getopt_long(argc, argv, "hv", longOptions, &optionIndex);
         if(c == -1) {
             break;
         }
 
         switch(c) {
-            case 'i':
-                args.localId = std::stoul(optarg);
-                break;
-
             case 'h':
-                printHelp(basename(argv[0]) );
+                printHelp(basename(argv[0]), cs2ContactData);
                 return true;
 
             case 'v':
                 moba::printAppData(appData);
                 return true;
 
-            case 'f':
-                args.function = std::stoul(optarg);
+            case 'c':
+                cs2ContactData.host = optarg;
                 break;
 
-            case 's':
-                args.speed = std::stoul(optarg);
+            case 'i':
+                cs2ContactData.portIn = std::stoi(optarg);
                 break;
+
+            case 'o':
+                cs2ContactData.portOut = std::stoi(optarg);
+                break;
+
+            default:
+                std::cerr << "Try '" << basename(argv[0]) << " --help' for more information." << std::endl;
+                exit(2);
         }
     }
     return false;
 }
 
-int main(const int argc, char** argv) {
-    CmdLineArguments args = {
-        0
+int main(int argc, char *argv[]) {
+    CS2ContactData cs2ContactData = {
+        "127.0.0.1",
+        CS2Reader::DEFAULT_PORT,
+        CS2Writer::DEFAULT_PORT
     };
 
-    if(parseArguments(argc, argv, args)) {
+    if(parseArguments(argc, argv, appData, cs2ContactData)) {
         return EXIT_SUCCESS;
     }
 
-    std::string action{argv[1]};
+    const auto cs2WriterPtr = std::make_shared<CS2Writer>(cs2ContactData.host, cs2ContactData.portIn);
+    const auto cs2ReaderPtr = std::make_shared<CS2Reader>(cs2ContactData.host, cs2ContactData.portOut, false);
 
-    Handler handler;
-
-    std::set<CanCommand> allowedCommands;
-    //if(cmd != "") {
-    //    allowedCommands.insert(getCommandFromString(cmd));
-    // }
-
-    try {
-        if(action == "s88") {
-            handler.printFeedBackAction();
-        } else if(action == "dump") {
-            handler.printCanCommands(allowedCommands);
-        } else if(action == "loklist") {
-            handler.printLoklist();
-        } else if(action == "lokstat") {
-            handler.printConfigList(Handler::LOKSTATUS);
-        } else if(action == "loks") {
-            handler.printConfigList(Handler::LOKLIST);
-        } else if(action == "magstat") {
-            handler.printConfigList(Handler::MAGSTATUS);
-        } else if(action == "mags") {
-            handler.printConfigList(Handler::MAGLIST);
-        } else if(action == "function_on") {
-            handler.setFunction(args.localId, args.function, true);
-        } else if(action == "function_off") {
-            handler.setFunction(args.localId, args.function, false);
-        } else if(action == "speed") {
-            handler.setSpeed(args.localId, args.speed);
-        } else if(action == "switch_green") {
-            handler.setSwitch(args.localId, true);
-        } else if(action == "switch_red") {
-            handler.setSwitch(args.localId, false);
-        } else {
-            std::cout << "unknown command <" << action << "> given!" << std::endl;
-        }
-    } catch(std::exception &e) {
-        std::cout << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+    auto app = Gtk::Application::create("org.moba.sniffer");
+    return app->make_window_and_run<FrmMain>(--argc, argv, cs2WriterPtr, cs2ReaderPtr);
 }
-
